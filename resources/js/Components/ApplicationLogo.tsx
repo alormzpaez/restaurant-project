f6b0b1fe847e4dba34cd48c4e4667c6ccb96f269
alormzpaez <alormzpaez@gmail.com>
2024-11-23 10:44:22 -0600

    Logo

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -7,7 +7,7 @@ export default function ApplicationLogo(props: SVGAttributes<SVGElement>) {
             <img
                 width={70}
                 height={70}
-                src="http://localhost/images/logo.png"
+                src="http://143.244.149.24/images/logo.png"
                 alt="image 1"
                 className=""
             />
